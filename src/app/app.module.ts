0c35d49c7c3676f5d0e4f5317dc50f0ca6f9af8d
Cristian <cristian123105@gmail.com>
2024-08-23 13:41:42 -0300

    chore: config project with PO UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { LoginService } from './login/login.service';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { PoModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { PoStorageModule  } from '@po-ui/ng-storage';
+
+@NgModule({
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    LoginModule,
+    HttpClientModule,
+    HomeModule,
+    BrowserAnimationsModule,
+    PoModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule,
+    PoStorageModule.forRoot()
+  ],
+  declarations: [AppComponent],
+  providers: [LoginService, 
+    provideHttpClient(withInterceptorsFromDi()),],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const environment = Object.freeze({
+  production: false,
+  API_URL: 'http://localhost:3000',
+});
