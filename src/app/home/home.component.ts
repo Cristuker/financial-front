b11f052d5c4c25b213653ab3025dfb7c24935ff9
Cristian <cristian123105@gmail.com>
2024-08-26 08:48:11 -0300

    feat: logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PoMenuItem } from '@po-ui/ng-components';
+import { PoStorageService } from '@po-ui/ng-storage';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +17,19 @@ export class HomeComponent {
       action: () => {
         this.router.navigate(['/home/clients']);
       }
+    },
+    {
+      label: 'Log out',
+      icon: 'ph ph-sign-out',
+      action: () => {
+        this.poStorageService.clear();
+        this.router.navigate(['/login']);
+      }
     }
   ];
 
-  constructor(private readonly router: Router) {}
+  constructor(private readonly router: Router, private readonly poStorageService: PoStorageService) {
+
+  }
 
 }
